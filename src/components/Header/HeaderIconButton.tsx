db8967084cffa5074bc8cd5210e423c3b044fe75
PenguinCho <ms0223900@gmail.com>
2021-07-17 15:29:31 +0800

    feat: add basic components

---
+++ src/components/Header/HeaderIconButton.tsx
@@ -0,0 +1,12 @@
+import React, { memo } from 'react';
+import { Box } from '@material-ui/core';
+
+const HeaderIconButton = () => {
+  return (
+    <Box>
+      
+    </Box>
+  );
+};
+
+export default memo(HeaderIconButton);
---
+++ src/components/Steps/StepStatus.tsx
@@ -0,0 +1,12 @@
+import React, { memo } from 'react';
+import { Box } from '@material-ui/core';
+
+const StepStatus = () => {
+  return (
+    <Box>
+
+    </Box>
+  );
+};
+
+export default memo(StepStatus);
